03adeefff658d4792e7bf1241103cc09dadd5c84
rheezam <ahnaf.handono.27@gmail.com>
2026-09-19 12:47:10 +0800

    Upload PT 2

---
+++ pertemuan-1.py
@@ -0,0 +1,41 @@
+# a = 5
+# b = 10
+# c = a + b
+# print(c)
+
+# a = 5.5
+# b = 10.10
+# c = a + b
+# print(c)
+
+# print("""Halo nama rendy
+# Saya suka belajar coding
+# Hari ini saya belajar bahasa pemrograman python""")
+
+# teks = "Rahmat Flowchart"
+
+# #menampilkan index ke-3
+# print(teks[3])
+# #menampilkan index ke-3 hingga ke-6
+# print(teks[3:6])
+# #menampilkan seluruhnya, tetapi dimulai dari index ke-7
+# print(teks[7:])
+# #menampilkan hanya sampai index ke-6
+# print(teks[:6])
+
+# mata_kuliah = ["APD","Logika Matematika","Kalkulus"]
+# print(mata_kuliah[0])
+# print(mata_kuliah[1])
+# print(mata_kuliah[2])
+
+# a = "20"
+# print(type(a))
+
+# nama = input("Masukkan nama anda : ")
+# print(nama)
+
+# a = 2
+# b = "20" 
+# c = a * b
+# print (c)
+
